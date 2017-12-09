52fef19a95eaf7de0084ba708afaba5454d9d29f
yunusalmaliq <yunusalmaliq@gmail.com>
2017-12-09 15:54:39 +0700

    tambah file

---
+++ desimal.py
@@ -0,0 +1,8 @@
+print("1.desimal ke biner")
+print("2.exit")
+i = int(input("masukan pilihan anda:"))
+if i ==1:
+	x = int(input("masukan angka desimal:"))
+	print(bin(x))
+else:
+	print("good bye")
